05cf26eb035a9661fa2cea3f204412c72be6dbe2
Luis <luisgns8022@gmail.com>
2026-06-06 18:32:11 -0300

    Outra estrutura para representar os dados

--- 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -17,6 +17,13 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
+    }
+
+
     public String getNome() {
         return nome;
     }
@@ -77,6 +84,7 @@ public class Titulo implements Comparable<Titulo> {
         return "Titulo{" +
                 "nome='" + nome + '\'' +
                 ", anoDeLancamento=" + anoDeLancamento +
+                ", duracaoEmMinutos=" + duracaoEmMinutos +
                 '}';
     }
 }
