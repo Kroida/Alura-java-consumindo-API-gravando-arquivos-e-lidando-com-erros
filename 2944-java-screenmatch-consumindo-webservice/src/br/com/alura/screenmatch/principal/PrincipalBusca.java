bc6049bd318f7bd84087de71164df8e5b83f4d23
Luis <luisgns8022@gmail.com>
2026-06-07 11:17:42 -0300

    Tratando diversas exceções

--- 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalBusca.java
+++ 2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalBusca.java
@@ -22,29 +22,34 @@ public class PrincipalBusca {
         // commit
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=bc05ef9c";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-
         try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+
+
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Titulo convertido");
             System.out.println(meuTitulo);
         } catch (NumberFormatException e) {
             System.out.println("Erro aqui aqui erro: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Algum erro de argumento na busca, verifique o endereço \n" + e.getMessage());
         }
+
+        System.out.println("O programa finalizou corretamente!");
     }
 }
